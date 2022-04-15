4c6ed3cc66a722ad6c142bf8008bedd88068c2dc
dolivare5 <deiverolivares2@gmail.com>
2022-04-15 13:07:33 -0500

    Modifique un poco el HTML y modifique y agregué nuevos estilos para que se vea bien en cualquier dispositivos.

--- build/js/index.js
+++ build/js/index.js
@@ -1,18 +1,20 @@
-let contenedor = document.querySelector('.barra__contenedor');
-let barra = document.querySelector('.barra');
-
+let contenedor = document.querySelector('.header__contenedor');
+let barra = document.querySelector('.header');
+let barra_busqueda = document.querySelector('.header__busqueda');
 window.onscroll = function () {
     let y = window.scrollY;
 
     console.log(y)
    if (y>=130){
-       barra.classList.add(('barra--fijar'));
-       contenedor.classList.add('barra--modificar');
+       barra.classList.add(('header--fijar'));
+       barra_busqueda.classList.add('header__busqueda--activar');
+       contenedor.classList.add('header--modificar');
        barra.classList.remove('contenedor');
    }else{
-       contenedor.classList.remove('barra--modificar');
-       barra.classList.remove(('barra--fijar'));
+       contenedor.classList.remove('header--modificar');
+       barra.classList.remove(('header--fijar'));
        barra.classList.add('contenedor');
+       barra_busqueda.classList.remove('header__busqueda--activar')
    }
 }
 
@@ -20,7 +22,7 @@ const ancho = window.innerWidth;
 const altura = window.innerHeight;
 
 if (ancho <=480){
-    const barra_busqueda = document.querySelector('.barra__campo-busqueda');
+    const barra_busqueda = document.querySelector('.header__campo-busqueda');
     barra_busqueda.placeholder = '¿A donde vas?';
 }
 
